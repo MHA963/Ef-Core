5470a8cbf855d533b1b658f9f0862b40f75f0b93
Hashem Abdou <MHA_SW@users.noreply.github.com>
2023-04-11 14:50:20 +0200

    adding packages and setting up the database

---
+++ CanteenDb/CanteenDb/myDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+
+namespace CanteenDb
+{
+    public class myDbContext : DbContext
+    {
+        public DbSet<Canteen> Canteen { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Ratings> Ratings { get; set; }
+        public DbSet<Meals> Meals { get; set; }
+        public DbSet<Reservations> Reservations { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=SqlConnection;Persist Security Info=True;User ID=sa;Password=220170Aa");
+        }
+
+    }
+}
