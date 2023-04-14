10a1a499e82fa16cda84bd06645ce82991fb6395
Hashem Abdou <MHA_SW@users.noreply.github.com>
2023-04-14 15:45:52 +0200

    ligemeget

--- CanteenDb/CanteenDb/Program.cs
+++ CanteenDb/CanteenDb/Program.cs
@@ -88,187 +88,187 @@ public class Program
         #endregion
 
         #region Pulling data from the database
-        using (var dbContext = new myDbContext()) // Replace with your actual DbContext name
-        {
+        //using (var dbContext = new myDbContext()) // Replace with your actual DbContext name
+        //{
 
-            // Input canteen name
-            string canteenName = "Canteen 1"; // Replace with the input canteen name
+        //    // Input canteen name
+        //    string canteenName = "Canteen 1"; // Replace with the input canteen name
 
-            // Query to get the menu options for the input canteen name
-            var menuOptions = dbContext.StreetFood
-                .Where(rm => rm.Canteen.CanteenName == canteenName)
-                .Select(rm => new
-                {
-                    rm.name
-                })
-                .ToList();
-            var menuOptions2 = dbContext.Warmdish
-                .Where(rm => rm.Canteen.CanteenName == canteenName)
-                .Select(rm => new
-                {
-                    rm.name
-                })
-                .ToList();
+        //    // Query to get the menu options for the input canteen name
+        //    var menuOptions = dbContext.StreetFood
+        //        .Where(rm => rm.Canteen.CanteenName == canteenName)
+        //        .Select(rm => new
+        //        {
+        //            rm.name
+        //        })
+        //        .ToList();
+        //    var menuOptions2 = dbContext.Warmdish
+        //        .Where(rm => rm.Canteen.CanteenName == canteenName)
+        //        .Select(rm => new
+        //        {
+        //            rm.name
+        //        })
+        //        .ToList();
 
-            // Join the two queries using Union
-            var combinedMenuOptions = menuOptions.Union(menuOptions2).ToList();
+        //    // Join the two queries using Union
+        //    var combinedMenuOptions = menuOptions.Union(menuOptions2).ToList();
 
-            // Output the menu options
-            Console.WriteLine($"Menu options for {canteenName}:");
-            foreach (var menuOption in combinedMenuOptions)
-            {
-                Console.WriteLine($"- Meal Name: {menuOption.name}");
-            }
+        //    // Output the menu options
+        //    Console.WriteLine($"Menu options for {canteenName}:");
+        //    foreach (var menuOption in combinedMenuOptions)
+        //    {
+        //        Console.WriteLine($"- Meal Name: {menuOption.name}");
+        //    }
 
-            Console.WriteLine("------------------------------------------------");
+        //    Console.WriteLine("------------------------------------------------");
 
-            // Define the CPR value for which you want to retrieve reservations
-            string cprValue = "000001"; // Replace with the actual CPR value
+        //    // Define the CPR value for which you want to retrieve reservations
+        //    string cprValue = "000001"; // Replace with the actual CPR value
 
-            // Query reservations based on CPR value
-            var reservations = dbContext.Reservation
-                .Where(r => r.CPR == cprValue)
-                .ToList();
+        //    // Query reservations based on CPR value
+        //    var reservations = dbContext.Reservation
+        //        .Where(r => r.CPR == cprValue)
+        //        .ToList();
 
-            // Output the reservations
-            Console.WriteLine($"Reservations for CPR: {cprValue}");
-            foreach (var reservation in reservations)
-            {
-                Console.WriteLine($"- Reservation ID: {reservation.ReservationId}");
-                Console.WriteLine($"- Canteen Name: {reservation.CanteenName}");
-                Console.WriteLine($"- Meal Name: {reservation.MealName}");
-                // Output other relevant reservation properties as needed
-            }
+        //    // Output the reservations
+        //    Console.WriteLine($"Reservations for CPR: {cprValue}");
+        //    foreach (var reservation in reservations)
+        //    {
+        //        Console.WriteLine($"- Reservation ID: {reservation.ReservationId}");
+        //        Console.WriteLine($"- Canteen Name: {reservation.CanteenName}");
+        //        Console.WriteLine($"- Meal Name: {reservation.MealName}");
+        //        // Output other relevant reservation properties as needed
+        //    }
 
-            Console.WriteLine("------------------------------------------------");
-
-
-            // Query reservations and group by MealName, counting the occurrences of each meal
-            var reservationCounts = dbContext.Reservationlist
-                .GroupBy(r => r.MealName)
-                .Select(g => new
-                {
-                    MealName = g.Key,
-                    Amount = g.Count()
-                })
-                .ToList();
-
-            // Output the meal counts
-            Console.WriteLine("Meal counts:");
-            foreach (var reservationCount in reservationCounts)
-            {
-                Console.WriteLine($"- Meal Name: {reservationCount.MealName}");
-                Console.WriteLine($"- Amount: {reservationCount.Amount}");
-            }
-
-            Console.WriteLine("------------------------------------------------");
+        //    Console.WriteLine("------------------------------------------------");
 
 
-            // Query JIT meal option from JIT table
-            var jitMealOption = dbContext.JITMeals
-                .Where(sf => sf.Canteen.CanteenName == canteenName)
-                .Select(sf => new
-                {
-                    sf.JITName
-                })
-                .FirstOrDefault();
+        //    // Query reservations and group by MealName, counting the occurrences of each meal
+        //    var reservationCounts = dbContext.Reservationlist
+        //        .GroupBy(r => r.MealName)
+        //        .Select(g => new
+        //        {
+        //            MealName = g.Key,
+        //            Amount = g.Count()
+        //        })
+        //        .ToList();
 
-            // Query available Canceled meals from Canceled table
-            var canceledMeals = dbContext.CanceledMeals
-                .Where(wd => wd.Canteen.CanteenName == canteenName)
-                .Select(wd => new
-                {
-                    wd.CanceledMealsName
-                })
-                .ToList();
+        //    // Output the meal counts
+        //    Console.WriteLine("Meal counts:");
+        //    foreach (var reservationCount in reservationCounts)
+        //    {
+        //        Console.WriteLine($"- Meal Name: {reservationCount.MealName}");
+        //        Console.WriteLine($"- Amount: {reservationCount.Amount}");
+        //    }
 
-            // Output the JIT meal option
-            Console.WriteLine($"JIT meal option for {canteenName}:");
-            if (jitMealOption != null)
-            {
-                Console.WriteLine($"- Meal Name: {jitMealOption.JITName}");
-            }
-            else
-            {
-                Console.WriteLine("No JIT meal option available.");
-            }
-
-            // Output the available Canceled meals
-            Console.WriteLine($"Available Canceled meals for {canteenName}:");
-            if (canceledMeals.Any())
-            {
-                foreach (var canceledMeal in canceledMeals)
-                {
-                    Console.WriteLine($"- Meal Name: {canceledMeal.CanceledMealsName}");
-                }
-            }
-            else
-            {
-                Console.WriteLine("No available Canceled meals.");
-            }
+        //    Console.WriteLine("------------------------------------------------");
 
 
+        //    // Query JIT meal option from JIT table
+        //    var jitMealOption = dbContext.JITMeals
+        //        .Where(sf => sf.Canteen.CanteenName == canteenName)
+        //        .Select(sf => new
+        //        {
+        //            sf.JITName
+        //        })
+        //        .FirstOrDefault();
 
-            Console.WriteLine("------------------------------------------------");
+        //    // Query available Canceled meals from Canceled table
+        //    var canceledMeals = dbContext.CanceledMeals
+        //        .Where(wd => wd.Canteen.CanteenName == canteenName)
+        //        .Select(wd => new
+        //        {
+        //            wd.CanceledMealsName
+        //        })
+        //        .ToList();
 
-            // Query available Canceled meals from Warmdish table in canteens except for Canteen1
-            var canceledMeals2 = dbContext.Warmdish
-                .Where(wd => wd.Canteen.CanteenName != canteenName)
-                .Select(wd => new
-                {
-                    CanteenName = wd.Canteen.CanteenName,
-                    MealName = wd.name
-                })
-                .ToList();
+        //    // Output the JIT meal option
+        //    Console.WriteLine($"JIT meal option for {canteenName}:");
+        //    if (jitMealOption != null)
+        //    {
+        //        Console.WriteLine($"- Meal Name: {jitMealOption.JITName}");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("No JIT meal option available.");
+        //    }
 
-            // Output the available Canceled meals
-            Console.WriteLine($"Available Canceled meals in canteens except for {canteenName}:");
-            if (canceledMeals.Any())
-            {
-                foreach (var canceledMeal in canceledMeals2)
-                {
-                    Console.WriteLine($"- Canteen Name: {canceledMeal.CanteenName}, Meal Name: {canceledMeal.MealName}");
-                }
-            }
-            else
-            {
-                Console.WriteLine("No available Canceled meals.");
-            }
+        //    // Output the available Canceled meals
+        //    Console.WriteLine($"Available Canceled meals for {canteenName}:");
+        //    if (canceledMeals.Any())
+        //    {
+        //        foreach (var canceledMeal in canceledMeals)
+        //        {
+        //            Console.WriteLine($"- Meal Name: {canceledMeal.CanceledMealsName}");
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("No available Canceled meals.");
+        //    }
 
 
-            Console.WriteLine("------------------------------------------------");
+
+        //    Console.WriteLine("------------------------------------------------");
+
+        //    // Query available Canceled meals from Warmdish table in canteens except for Canteen1
+        //    var canceledMeals2 = dbContext.Warmdish
+        //        .Where(wd => wd.Canteen.CanteenName != canteenName)
+        //        .Select(wd => new
+        //        {
+        //            CanteenName = wd.Canteen.CanteenName,
+        //            MealName = wd.name
+        //        })
+        //        .ToList();
+
+        //    // Output the available Canceled meals
+        //    Console.WriteLine($"Available Canceled meals in canteens except for {canteenName}:");
+        //    if (canceledMeals.Any())
+        //    {
+        //        foreach (var canceledMeal in canceledMeals2)
+        //        {
+        //            Console.WriteLine($"- Canteen Name: {canceledMeal.CanteenName}, Meal Name: {canceledMeal.MealName}");
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("No available Canceled meals.");
+        //    }
+
+
+        //    Console.WriteLine("------------------------------------------------");
             
-            // Query and calculate average ratings from all canteens
-            var averageRatings = dbContext.Ratings
-                .GroupBy(r => r.CanteenName)
-                .Select(g => new
-                {
-                    CanteenName = g.Key,
-                    AverageRating = g.Average(r => r.Rating)
-                })
-                .OrderByDescending(g => g.AverageRating)
-                .ToList();
+        //    // Query and calculate average ratings from all canteens
+        //    var averageRatings = dbContext.Ratings
+        //        .GroupBy(r => r.CanteenName)
+        //        .Select(g => new
+        //        {
+        //            CanteenName = g.Key,
+        //            AverageRating = g.Average(r => r.Rating)
+        //        })
+        //        .OrderByDescending(g => g.AverageRating)
+        //        .ToList();
 
-            // Output the average ratings
-            Console.WriteLine("Average Ratings from all canteens:");
-            if (averageRatings.Any())
-            {
-                foreach (var rating in averageRatings)
-                {
-                    Console.WriteLine($"- Canteen Name: {rating.CanteenName}, Average Rating: {rating.AverageRating}");
-                }
-            }
-            else
-            {
-                Console.WriteLine("No ratings found.");
-            }
-
-
+        //    // Output the average ratings
+        //    Console.WriteLine("Average Ratings from all canteens:");
+        //    if (averageRatings.Any())
+        //    {
+        //        foreach (var rating in averageRatings)
+        //        {
+        //            Console.WriteLine($"- Canteen Name: {rating.CanteenName}, Average Rating: {rating.AverageRating}");
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("No ratings found.");
+        //    }
 
 
 
 
-        }
+
+
+        //}
 
         #endregion
 
